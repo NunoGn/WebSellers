8811c1ea326c88bbdc1156b81b94eeb7f3c8d807
Nuno Correia <nunogncorreia25@gmail.com>
2020-06-11 16:00:35 +0100

    Criacao do Crud Scaffolding

---
+++ WebApp_SellersTable/Data/WebApp_SellersTableContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp_SellersTable.Models;
+
+namespace WebApp_SellersTable.Data
+{
+    public class WebApp_SellersTableContext : DbContext
+    {
+        public WebApp_SellersTableContext (DbContextOptions<WebApp_SellersTableContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp_SellersTable.Models.Department> Department { get; set; }
+    }
+}
--- WebApp_SellersTable/Startup.cs
+++ WebApp_SellersTable/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebApp_SellersTable.Data;
 
 namespace WebApp_SellersTable
 {
@@ -33,6 +35,9 @@ namespace WebApp_SellersTable
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApp_SellersTableContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApp_SellersTableContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
