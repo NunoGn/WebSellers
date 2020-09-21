72934c29dba0b1b6a22627972c84228070056337
Nuno Correia <nunogncorreia25@gmail.com>
2020-09-21 19:03:38 +0100

    Exception handling for delete

---
+++ WebApp_SellersTable/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApp_SellersTable.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
